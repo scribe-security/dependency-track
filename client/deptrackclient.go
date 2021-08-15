fc678da526a0aaed8f0750e31a243cedafbc69cb
Ido Shabi <ido@scribesecurity.com>
2021-08-15 17:40:30 +0300

    yq

--- client/deptrackclient.go
+++ client/deptrackclient.go
@@ -3,6 +3,10 @@ package client
 import (
 	"bytes"
 	"encoding/json"
+<<<<<<< Updated upstream
+=======
+	"fmt"
+>>>>>>> Stashed changes
 	"io/ioutil"
 	"net/url"
 	"strings"
@@ -33,6 +37,31 @@ type DepTrackSbomPost struct {
 	ProjectVersion string `json:"projectVersion,omitempty"`
 }
 
+<<<<<<< Updated upstream
+=======
+type LatestVersionParams struct {
+	Purl string `json:"purl,omitempty"`
+}
+
+type GetComponentByPurlParams struct {
+	Purl string `json:"purl,omitempty"`
+}
+
+type GetVulnerabilityByUUIDParams struct {
+	Suppressed bool `json:"suppressed,omitempty"`
+}
+
+type LatestVersionResponse struct {
+	RepositoryType string `json:"repositoryType,omitempty"`
+	Namespace      string `json:"namespace,omitempty"`
+	Name           string `json:"name,omitempty"`
+	LatestVersion  string `json:"latestVersion,omitempty"`
+	// TBD: Add support to time parsing
+	// Published      time.Time `json:"published,omitempty"`
+	// LastCheck      time.Time `json:"lastCheck,omitempty"`
+}
+
+>>>>>>> Stashed changes
 type DepTrackSbomPostResponse struct {
 	Token string `json:"token"`
 	Test  string
@@ -44,6 +73,50 @@ type DepTrackClient struct {
 	accessToken string
 }
 
+<<<<<<< Updated upstream
+=======
+type Cwe struct {
+	cweId int    `json:"vulnId,omitempty"`
+	name  string `json:"name,omitempty"`
+}
+
+type Vulnraibility struct {
+	VulnId          string  `json:"vulnId,omitempty"`
+	Source          string  `json:"source,omitempty"`
+	Description     string  `json:"description,omitempty"`
+	CvssV2BaseScore float64 `json:"cvssV2BaseScore,omitempty"`
+	CvssV3BaseScore float64 `json:"cvssV3BaseScore,omitempty"`
+	Cwe             Cwe     `json:"cwe,omitempty"`
+	References      string  `json:"references,omitempty"`
+	Severity        string  `json:"severity,omitempty"`
+	// TBD: Add support to time parsing
+	//Published       string  `json:"published,omitempty"`
+	//Updated         time.Time `json:"updated,omitempty"`
+}
+
+type Component struct {
+	Author    string `json:"author,omitempty"`
+	Publisher string `json:"publisher,omitempty"`
+	Group     string `json:"group,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Version   string `json:"version,omitempty"`
+	Filename  string `json:"filename,omitempty"`
+	Extension string `json:"extension,omitempty"`
+	Md5       string `json:"md5,omitempty"`
+	Sha1      string `json:"sha1,omitempty"`
+	Sha256    string `json:"sha256,omitempty"`
+	Cpe       string `json:"cpe,omitempty"`
+	Purl      Purl   `json:"-,omitempty"`
+	UUID      string `json:"uuid,omitempty"`
+}
+
+type ComponentList []Component
+type VulnraibilityList []Vulnraibility
+
+const GetAllVulnerabilities string = "/vulnerability/component"
+const GetAllComponent string = "/component/identity"
+
+>>>>>>> Stashed changes
 var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
 
 func escapeQuotes(s string) string {
@@ -131,3 +204,64 @@ func (depClient *DepTrackClient) PostSbom(api string, deptrack_params *DepTrackS
 
 	return err
 }
+<<<<<<< Updated upstream
+=======
+
+func (depClient *DepTrackClient) GetRepositoryLatest(PURL string) (*LatestVersionResponse, error) {
+	const LatestApiPath string = "/repository/latest"
+	var latestVersion LatestVersionResponse
+	params := LatestVersionParams{Purl: PURL}
+
+	err := depClient.GetJsonWithParams(LatestApiPath, params, &latestVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	return &latestVersion, nil
+}
+
+func (depClient *DepTrackClient) GetComponentByPURL(PURL string) (ComponentList, error) {
+	var component_list ComponentList
+	params := GetComponentByPurlParams{Purl: PURL}
+
+	if err := depClient.GetJsonWithParams(GetAllComponent, params, &component_list); err != nil {
+		return nil, err
+	}
+
+	for _, com := range component_list {
+		fmt.Printf("%+v\n", com)
+	}
+
+	return component_list, nil
+}
+
+func (depClient *DepTrackClient) GetVulnerabilityComponenetByUUID(uuid string, isSupported bool) (VulnraibilityList, error) {
+
+	var vulnraibilityList VulnraibilityList
+	//params := GetVulnerabilityByUUIDParams{Suppressed: true}
+	FullVulnrabilityPath := GetAllVulnerabilities + "/" + uuid
+
+	if err := depClient.GetJson(FullVulnrabilityPath, &vulnraibilityList); err != nil {
+		return nil, err
+	}
+
+	for _, com := range vulnraibilityList {
+		fmt.Printf("%+v\n", com)
+	}
+
+	return vulnraibilityList, nil
+}
+
+func (depClient *DepTrackClient) GetLatestVersion(PURL string) (float64, float64, error) {
+
+	latest, err := depClient.GetRepositoryLatest(PURL)
+	if err != nil {
+		return 0, 0, err
+	}
+	LatestVersion := latest.LatestVersion
+	PURL.
+		purl = NewPurl(PURL)
+	resp = GetRepositoryLatest(purl.raw)
+	return purl.version, LatestVersion
+}
+>>>>>>> Stashed changes
